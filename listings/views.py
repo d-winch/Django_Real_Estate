3331c7f2d0bc2646e670a926ae9d398e81d19c1f
winch_ <d.winchy@gmail.com>
2019-03-03 16:13:59 +0000

    Pagination and filter of published listings

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
-
+    listings_list = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings_list, 3)  # Show 3 listings per page
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
