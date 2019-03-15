9b455903e6e9614af1f0b72f5263a8a093c54192
winch_ <d.winchy@gmail.com>
2019-03-15 13:59:47 +0000

    Aded search functionality

--- listings/views.py
+++ listings/views.py
@@ -24,10 +24,47 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords) # icontains - If contains keyword
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city) # iexact - Case insensitive exact match
+    
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)  # iexact - Case insensitive exact match
+    
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)  # lte - Less than or equal to
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)  # lte - Less than or equal to
+
     context = {
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
         'state_choices': state_choices,
+        'listings': queryset_list
     }
 
     return render(request, 'listings/search.html', context)
