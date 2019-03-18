d7f2dde3a7ae15d356303e53ec9785d896375b7c
winch_ <d.winchy@gmail.com>
2019-03-18 23:17:07 +0000

    Validate and register a user

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,48 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'TESTING ERROR MESSAGE')
+        # Get POST values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check is passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(
+                    request, 'That username is taken, please use a different one')
+                return redirect('register')
+            else:
+                # Check Email
+                if User.objects.filter(email=email).exists():
+                    messages.error(
+                        request, 'That email is being used, please use a different one')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Log in after registering the user
+                    #auth.login(request, user)
+                    #messages.success(request, 'You are now logged in')
+                    #return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registered and can log in')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
         return redirect('register')
     else:
         return render(request, 'accounts/register.html')
+
 
 def login(request):
     if request.method == 'POST':
@@ -15,8 +51,10 @@ def login(request):
     else:
         return render(request, 'accounts/login.html')
 
+
 def logout(request):
     return redirect('index')
+
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
