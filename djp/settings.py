4cf56cdf9b921175e0ed860a810bf689943bea69
winch_ <d.winchy@gmail.com>
2019-11-02 16:05:41 +0000

    Changed allowed hosts

--- djp/settings.py
+++ djp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = settings_secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['real-estate.davidwin.ch']
+ALLOWED_HOSTS = ['127.0.0.1', 'real-estate.davidwin.ch', '46.101.192.188']
 
 
 # Application definition
